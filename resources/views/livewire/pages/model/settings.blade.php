1d17fc79d3a625cf118201b5aaf71794cda25371
Raihan <17radf@gmail.com>
2025-01-14 00:37:23 +0700

    feat: update retraining to sync species list

--- resources/views/livewire/pages/model/settings.blade.php
+++ resources/views/livewire/pages/model/settings.blade.php
@@ -1,4 +1,9 @@
 <?php
+
+use App\Models\Species;
+use Livewire\Attributes\On;
+
+
 use Livewire\Volt\Component;
 use Illuminate\Support\Facades\Http;
 
@@ -20,6 +25,7 @@ new class extends Component {
             if ($response->successful()) {
                 $this->retrainingOutput = $data['details'] ?? $data['message'];
                 $this->hasError = false;
+                $this->dispatch('retrain-success');
             } else {
                 $this->retrainingOutput = $data['error'] ?? 'An unknown error occurred';
                 $this->hasError = true;
@@ -29,6 +35,36 @@ new class extends Component {
             $this->hasError = true;
         } finally {
             $this->isRetraining = false;
+        }
+    }
+
+    #[On('retrain-success')]
+    public function getSpecies()
+    {
+        try {
+            // get species from API
+            $response = Http::get('http://127.0.0.1:8000/species');
+
+            if ($response->successful()) {
+                $species = $response->json();
+
+                // loop through the species from the API
+                foreach ($species as $speciesName) {
+                    // check if the species already exists in the database
+                    if (!Species::where('name', $speciesName)->exists()) {
+                        // insert the species if it does not exist
+                        Species::create(['name' => $speciesName]);
+                    }
+                }
+
+                $this->syncMessage = 'Species list successfully synchronized.';
+            } else {
+                throw new \Exception('Failed to fetch species from API');
+            }
+        } catch (\Exception $e) {
+        dd($e);
+            $this->syncMessage = 'Failed to synchronize species list: ' . $e->getMessage();
+            $this->hasError = true;
         }
     }
 }; ?>
