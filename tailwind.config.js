a7fe607e7ca2e145b8216e073cdd19033b9dccfe
Raihan <17radf@gmail.com>
2025-01-17 21:08:40 +0700

    chore: idk what happened, something wrong about filament notification package

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,6 @@ export default {
     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     './storage/framework/views/*.php',
     './resources/views/**/*.blade.php',
-    './app/Filament/**/*.php',
-    './resources/views/filament/**/*.blade.php',
-    './vendor/filament/**/*.blade.php',
   ],
 
   theme: {
